5f64e72eaff222a2e21ea192c7a51f2d30f6adc3
superbaiao1208 <1299478311@qq.com>
2021-03-10 22:29:29 +0800

    快速选择

---
+++ 排序/AcWing786.第k个数.cpp
@@ -0,0 +1,41 @@
+//题目链接：
+https://www.acwing.com/problem/content/788/
+
+//代码实现：
+#include <iostream>
+
+using namespace std;
+
+const int N = 100010;
+
+int q[N];
+
+int quick_sort(int q[], int l, int r, int k)
+{
+    if (l >= r) return q[l];
+
+    int i = l - 1, j = r + 1, x = q[l + r >> 1];
+    while (i < j)
+    {
+        do i ++ ; while (q[i] < x);
+        do j -- ; while (q[j] > x);
+        if (i < j) swap(q[i], q[j]);
+    }
+
+    if (j - l + 1 >= k) return quick_sort(q, l, j, k);
+    else return quick_sort(q, j + 1, r, k - (j - l + 1));
+}
+
+int main()
+{
+    int n, k;
+    scanf("%d%d", &n, &k);
+
+    for (int i = 0; i < n; i ++ ) scanf("%d", &q[i]);
+
+    cout << quick_sort(q, 0, n - 1, k) << endl;
+
+    return 0;
+}
+
+
