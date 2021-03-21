c7a803bbeeccde38c8e5e892009e7236f7645ae5
superbaiao1208 <1299478311@qq.com>
2021-03-21 22:25:59 +0800

    二叉树

---
+++ 剑指offer第二版/44.分行从上往下打印二叉树.cpp
@@ -0,0 +1,43 @@
+//题目链接：
+https://leetcode-cn.com/problems/cong-shang-dao-xia-da-yin-er-cha-shu-ii-lcof/
+
+//代码实现：
+class Solution {
+public:
+    vector<vector<int>> levelOrder(TreeNode* root) 
+    {
+        vector<vector<int>>res;
+        if(!root) return res;
+        queue<TreeNode*>q;
+        
+        q.push(root);
+        q.push(nullptr);
+        
+        vector<int>level;//遍历树的层级
+        
+        while(q.size())
+        {
+            auto t=q.front();
+            q.pop();
+            //如果说遍历完一整行了
+            if(!t)
+            {
+                //如果说level空了 说明遍历完所有结果了 eg：下一层没有节点了
+                if(level.empty()) break;
+                //否则就把level放到答案里面去并把level清空
+                res.push_back(level);
+                level.clear();
+                q.push(nullptr);
+                continue;
+                
+            }
+            //否则就要扩展
+            //先把当前的点加到level里面去
+            level.push_back(t->val);
+            if(t->left) q.push(t->left);
+            if(t->right) q.push(t->right);
+        }
+        return res;
+
+    }
+};
