1be06b94973a7e7fe54946d7b956a55315731410
superbaiao1208 <1299478311@qq.com>
2021-03-23 17:47:02 +0800

    dfs

---
+++ 剑指offer第二版/23.矩阵中的路径.cpp
@@ -0,0 +1,32 @@
+//题目链接：
+https://www.acwing.com/problem/content/21/
+
+//代码实现：
+
+class Solution {
+public:
+    bool hasPath(vector<vector<char>>& matrix, string str) {
+        for (int i = 0; i < matrix.size(); i ++ )
+            for (int j = 0; j < matrix[i].size(); j ++ )
+                if (dfs(matrix, str, 0, i, j))
+                    return true;
+        return false;
+    }
+
+    bool dfs(vector<vector<char>> &matrix, string &str, int u, int x, int y) {
+        if (matrix[x][y] != str[u]) return false;
+        if (u == str.size() - 1) return true;
+        int dx[4] = {-1, 0, 1, 0}, dy[4] = {0, 1, 0, -1};
+        char t = matrix[x][y];
+        matrix[x][y] = '*';
+        for (int i = 0; i < 4; i ++ ) {
+            int a = x + dx[i], b = y + dy[i];
+            if (a >= 0 && a < matrix.size() && b >= 0 && b < matrix[a].size()) {
+                if (dfs(matrix, str, u + 1, a, b)) return true;
+            }
+        }
+        matrix[x][y] = t;
+        return false;
+    }
+};
+
